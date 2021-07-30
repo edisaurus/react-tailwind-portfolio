13a3bedd8d6d63a46feb85a05be78b0edffa19f7
edisaurus <edswork.ece@gmail.com>
2021-07-30 17:20:54 +0800

    add tailwind design to components

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,1 +1,36 @@
-export default function Skills() {}
+// import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
+import React from "react";
+// import { skills } from "../data";
+
+export default function Skills() {
+  return (
+    <section id="skills">
+      <div className="container px-5 py-10 mx-auto">
+        <div className="text-center mb-20">
+          {/* <ChipIcon /> */}
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+            Skills &amp; Technologies
+          </h1>
+          <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
+            ipsa delectus eum quo voluptas aspernatur accusantium distinctio
+            possimus est.
+          </p>
+        </div>
+        skills
+        {/* <div>
+          {skills.map((skill) => (
+            <div key={skill}>
+              <div>
+                <BadgeCheckIcon />
+                <span>
+                  {skill}
+                </span>
+              </div>
+            </div>
+          ))}
+        </div> */}
+      </div>
+    </section>
+  );
+}
